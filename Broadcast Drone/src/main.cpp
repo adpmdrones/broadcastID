d1144364d8841f0d5f91aa0e38d961571b6d4026
bitsMaster <hafizwaqasqammar@gmail.com>
2020-02-17 18:02:08 +0500

    Created project and add the sim800l interface and gps interface

---
+++ Broadcast Drone/src/main.cpp
@@ -0,0 +1,168 @@
+#include <Arduino.h>
+#include <Wire.h> //Needed for I2C to GPS
+#include <MicroNMEA.h>
+#include "SparkFun_Ublox_Arduino_Library.h"
+
+// Configure TinyGSM library
+#define TINY_GSM_MODEM_SIM800      // Modem is SIM800
+#define TINY_GSM_RX_BUFFER   1024  // Set RX buffer to 1Kb
+
+#include <TinyGsmClient.h>
+
+
+
+
+/********************************** Private Defines **********************/
+
+// Set serial for AT commands (to SIM800 module)
+#define SerialAT Serial1
+#define IP5306_ADDR          0x75
+#define IP5306_REG_SYS_CTL0  0x00
+
+// TTGO T-Call pins
+#define MODEM_RST            5
+#define MODEM_PWKEY          4
+#define MODEM_POWER_ON       23
+#define MODEM_TX             27
+#define MODEM_RX             26
+#define I2C_SDA              21
+#define I2C_SCL              22
+// Zoe-M8Q pins
+#define I2C_SDA_2            18
+#define I2C_SCL_2            19
+
+//////////////////////////////////////////////////////////////////////////
+
+/********************************* Global Variables *********************/
+
+// Your GPRS credentials (leave empty, if not needed)
+const char apn[]      = ""; // APN (example: internet.vodafone.pt) use https://wiki.apnchanger.org
+const char gprsUser[] = ""; // GPRS User
+const char gprsPass[] = ""; // GPRS Password
+
+// SIM card PIN (leave empty, if not defined)
+const char simPIN[]   = ""; 
+
+// Server details
+// The server variable can be just a domain name or it can have a subdomain. It depends on the service you are using
+const char server[] = "example.com"; // domain name: example.com, maker.ifttt.com, etc
+const char resource[] = "/post-data.php";         // resource path, for example: /post-data.php
+const int  port = 80;                             // server port number
+
+// Keep this API Key value to be compatible with the PHP code provided in the project page. 
+// If you change the apiKeyValue value, the PHP file /post-data.php also needs to have the same key 
+String apiKeyValue = "tPmAT5Ab3j7F9";
+
+char nmeaBuffer[100];
+
+
+SFE_UBLOX_GPS myGPS;
+MicroNMEA nmea(nmeaBuffer, sizeof(nmeaBuffer));
+TinyGsm modem(SerialAT);
+
+// I2C for SIM800 (to keep it running when powered from battery)
+TwoWire I2CPower = TwoWire(0);
+
+// I2C for zoe-M8Q sensor
+TwoWire I2CSensors = TwoWire(1);
+
+// TinyGSM Client for Internet connection
+TinyGsmClient gsmClient(modem);
+/////////////////////////////////////////////////////////////////////
+
+/****************************** Function Prototypes *****************/
+bool setPowerBoostKeepOn(int en);
+
+//////////////////////////////////////////////////////////////////////
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Starting Application");
+
+
+  // Start I2C communication
+  I2CPower.begin(I2C_SDA, I2C_SCL, 400000);
+  I2CSensors.begin(I2C_SDA_2, I2C_SCL_2, 400000);
+
+  // Keep power when running from battery
+  bool isOk = setPowerBoostKeepOn(1);
+  Serial.println(String("IP5306 KeepOn ") + (isOk ? "OK" : "FAIL"));
+
+  // Set modem reset, enable, power pins
+  pinMode(MODEM_PWKEY, OUTPUT);
+  pinMode(MODEM_RST, OUTPUT);
+  pinMode(MODEM_POWER_ON, OUTPUT);
+  digitalWrite(MODEM_PWKEY, LOW);
+  digitalWrite(MODEM_RST, HIGH);
+  digitalWrite(MODEM_POWER_ON, HIGH);
+
+  // Set GSM module baud rate and UART pins
+  SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
+  delay(3000);
+
+  // Restart SIM800 module, it takes quite some time
+  // To skip it, call init() instead of restart()
+  Serial.println("Initializing modem...");
+  modem.restart();
+
+  // Unlock your SIM card with a PIN if needed
+  if (strlen(simPIN) && modem.getSimStatus() != 3 ) {
+    modem.simUnlock(simPIN);
+  }
+  
+
+
+  if (myGPS.begin(I2CSensors, 0x42) == false)
+  {
+    Serial.println(F("Ublox GPS not detected at default I2C address. Please check wiring. Freezing."));
+    while (1);
+  }
+}
+
+void loop()
+{
+  myGPS.checkUblox(); //See if new data is available. Process bytes as they come in.
+
+  if(nmea.isValid() == true)
+  {
+    long latitude_mdeg = nmea.getLatitude();
+    long longitude_mdeg = nmea.getLongitude();
+
+    Serial.print("Latitude (deg): ");
+    Serial.println(latitude_mdeg / 1000000., 6);
+    Serial.print("Longitude (deg): ");
+    Serial.println(longitude_mdeg / 1000000., 6);
+  }
+  else
+  {
+    Serial.print("No Fix - ");
+    Serial.print("Num. satellites: ");
+    Serial.println(nmea.getNumSatellites());
+  }
+
+  delay(250); //Don't pound too hard on the I2C bus
+}
+
+//This function gets called from the SparkFun Ublox Arduino Library
+//As each NMEA character comes in you can specify what to do with it
+//Useful for passing to other libraries like tinyGPS, MicroNMEA, or even
+//a buffer, radio, etc.
+void SFE_UBLOX_GPS::processNMEA(char incoming)
+{
+  //Take the incoming char from the Ublox I2C port and pass it on to the MicroNMEA lib
+  //for sentence cracking
+  nmea.process(incoming);
+}
+
+
+bool setPowerBoostKeepOn(int en) 
+{
+  I2CPower.beginTransmission(IP5306_ADDR);
+  I2CPower.write(IP5306_REG_SYS_CTL0);
+  if (en) {
+    I2CPower.write(0x37); // Set bit1: 1 enable 0 disable boost keep on
+  } else {
+    I2CPower.write(0x35); // 0x37 is default reg value
+  }
+  return I2CPower.endTransmission() == 0;
+}
